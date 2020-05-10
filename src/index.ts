4a6ca0e18c50f120f7639ee48faad47095e46d5e
Yash Shah <yash@pop-os.localdomain>
2020-05-10 19:38:01 +0530

    package only exports default

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import LinkedList from './linked-list';
 import Queue from './queue';
 import Stack from './stack';
-export { LinkedList, Queue, Stack };
 export default { LinkedList, Queue, Stack };
